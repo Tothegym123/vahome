662c90294c50f70ace51636deaf47c5584197b75
Tothegym123 <tom@vahomes.com>
2026-04-29 07:25:51 -0400

    fix(redirects): remove catch-all /military-resources rule causing loop

--- next.config.js
+++ next.config.js
@@ -41,7 +41,6 @@ const nextConfig = {
   async redirects() {
     return [
     { source: '/military-resources', destination: '/military', permanent: true },
-    { source: '/military-resources/:path*', destination: '/military/:path*', permanent: true },
       {
         source: '/blog/Inspection-vs--Appraisal-for-Home-Buyer',
         destination: '/blog/Inspection-vs--Appraisal-for-Home-Buyers',
